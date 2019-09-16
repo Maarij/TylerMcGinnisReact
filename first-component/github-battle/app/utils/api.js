a6e9f068fc606c8bc95755e0899a91e414f37e50
Maarij Anwar <maarij.anwar@gmail.com>
2019-09-15 19:52:25 -0500

    Implement a promise to async/await refactor (outside of course) Repositories Grid

--- first-component/github-battle/app/utils/api.js
+++ first-component/github-battle/app/utils/api.js
@@ -1,13 +1,21 @@
+async function getRepos(language) {
+    try {
+        const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+        let response = await fetch(endpoint);
+        let reposData = await response.json();
+
+        if(!reposData.items) {
+            throw new Error(reposData.message)
+        }
+
+        return reposData.items;
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
 export function fetchPopularRepos (language) {
-    const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
-
-    return fetch(endpoint)
-        .then((res) => res.json())
-        .then((data) => {
-            if (!data.items) {
-                throw new Error(data.message)
-            }
-
-            return data.items
-        })
+    return getRepos(language);
 }
