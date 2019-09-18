bbcfdba4f37bd9a3245054aa45598ef68b2b0d11
Maarij <maarij.anwar@gmail.com>
2019-09-18 18:59:48 -0500

    Github API calls

--- first-component/github-battle/app/utils/api.js
+++ first-component/github-battle/app/utils/api.js
@@ -1,4 +1,69 @@
-async function getRepos(language) {
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getErrorMessage(message, username) {
+    if(message === 'Not Found') {
+        return `${username} does not exist`
+    }
+
+    return message
+}
+
+function getProfile(username) {
+    return fetch(`https://api.github.com/users/${username}${params}`)
+        .then((res) => res.json())
+        .then((profile) => {
+            if(profile.message) {
+                throw new Error(getErrorMessage(profile.message, username))
+            }
+
+            return profile;
+        })
+}
+
+function getRepos(username) {
+    return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+        .then((res) => res.json())
+        .then((repos) => {
+            if (repos.message) {
+                throw new Error(getErrorMessage(repos.message, username))
+            }
+
+            return repos
+        })
+}
+
+function getStarCount(repos) {
+    return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function calculateScore(followers, repos) {
+    return (followers * 3) + getStarCount(repos)
+}
+
+function getUserData(player) {
+    return Promise.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(([ profile, repos ]) => ({
+        profile
+        ,score: calculateScore(profile.followers, repos)
+    }))
+}
+
+function sortPlayers(players) {
+    return players.sort((a,b) => b.score - a.score)
+}
+
+export function battle(players) {
+    return Promise.all([
+        getUserData(players[0])
+        ,getUserData(players[1])
+    ]).then((results) => sortPlayers(results))
+}
+
+async function getReposAsync(language) {
     try {
         const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
 
@@ -17,5 +82,5 @@ async function getRepos(language) {
 }
 
 export function fetchPopularRepos (language) {
-    return getRepos(language);
+    return getReposAsync(language);
 }
